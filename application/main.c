44143388c56f782acda7fab5b71a720ea3d9cb02
vsfos <vsfos@foxmail.com>
2022-12-31 18:56:18 +0800

    [application][project][board] add

---
+++ application/main.c
@@ -0,0 +1,58 @@
+/*****************************************************************************
+ *   Copyright(C)2009-2022 by VSF Team                                       *
+ *                                                                           *
+ *  Licensed under the Apache License, Version 2.0 (the "License");          *
+ *  you may not use this file except in compliance with the License.         *
+ *  You may obtain a copy of the License at                                  *
+ *                                                                           *
+ *     http://www.apache.org/licenses/LICENSE-2.0                            *
+ *                                                                           *
+ *  Unless required by applicable law or agreed to in writing, software      *
+ *  distributed under the License is distributed on an "AS IS" BASIS,        *
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. *
+ *  See the License for the specific language governing permissions and      *
+ *  limitations under the License.                                           *
+ *                                                                           *
+ ****************************************************************************/
+
+/*============================ INCLUDES ======================================*/
+
+#include <unistd.h>
+#include "vsf_board.h"
+
+/*============================ MACROS ========================================*/
+/*============================ MACROFIED FUNCTIONS ===========================*/
+/*============================ TYPES =========================================*/
+/*============================ GLOBAL VARIABLES ==============================*/
+/*============================ LOCAL VARIABLES ===============================*/
+/*============================ PROTOTYPES ====================================*/
+/*============================ IMPLEMENTATION ================================*/
+
+int vsf_linux_create_fhs(void)
+{
+    // 0. devfs, busybox, etc
+    vsf_linux_vfs_init();
+    busybox_install();
+
+    // 1. driver
+    // 2. fs
+    // 3. app
+    return 0;
+}
+
+int VSF_USER_ENTRY(void)
+{
+    vsf_board_init();
+
+#if VSF_USE_TRACE == ENABLED
+    vsf_start_trace();
+#endif
+
+    vsf_linux_init(&(vsf_linux_stdio_stream_t){
+        .in     = (vsf_stream_t *)&VSF_DEBUG_STREAM_RX,
+        .out    = (vsf_stream_t *)&VSF_DEBUG_STREAM_TX,
+        .err    = (vsf_stream_t *)&VSF_DEBUG_STREAM_TX,
+    });
+
+    return 0;
+}
